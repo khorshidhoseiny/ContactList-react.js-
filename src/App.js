b40165d206c2f6bd4b793f195e23638ef6bee1dc
khorshidHoseiny <www.khorshid11@gmail.com>
2022-02-11 19:48:17 +0330

    create project simple tempelet

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+	return <div className="App">
+    <h1>contact-list</h1>
+    <section>form </section>
+    <section>contact list</section>
+  </div>;
+}
+
+export default App;
